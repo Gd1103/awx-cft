c9685c28d94efeab13e75f6ee4fd859bf81e3b91
vsmighty03 <devops.mar2.2019@gmail.com>
2020-12-22 18:26:03 +0530

    updated

--- sidecar.sh
+++ sidecar.sh
@@ -3,7 +3,6 @@ echo "FROM ansible/awx:16.0.0" > Dockerfile
 echo "VOLUME /etc/tower/" >> Dockerfile
 echo "VOLUME /etc/nginx/" >> Dockerfile
 echo "VOLUME /usr/local/etc/redis/" >> Dockerfile
-
 echo "COPY --chown=1000 credentials.py /etc/tower/conf.d/credentials.py" >> Dockerfile
 echo "COPY --chown=1000 SECRET_KEY /etc/tower/SECRET_KEY" >> Dockerfile
 echo "COPY --chown=1000 environment.sh /etc/tower/conf.d/environment.sh" >> Dockerfile
